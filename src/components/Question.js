fa7da08582719e426441d146e43b8f24c1f483bd
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-27 11:17:53 -0500

    commit tree 30c3a8fa9781884c27040270ee65f1f24b1a08fd

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10); // reset time
+      onAnswered(false); // answer incorrect if time runs out
+      return; // exit 
+    }
+
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000); // every 1 second, decrease time by 1
+
+    return () => {
+      clearTimeout(timer); //cleanup after timeout 
+    }
+  }, [timeRemaining, onAnswered]); // deps array - run this when timeRemaining changes, and when answered
+  // makes it so if answered and question changes, time resets, or when time counts down to continue countdown til 0
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
